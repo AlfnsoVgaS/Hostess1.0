d9ac3f84f1223697708b0e6f36a7a72009eea7f4
AlfnsoVgaS <alfonsovegasanchez@gmail.com>
2021-04-17 17:47:47 -0500

    Login-firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRsfeWUBi6hnXS1jaGojG9J8XJ6SgvhkY",
+    authDomain: "autenticacion-demo-aa645.firebaseapp.com",
+    databaseURL: "https://autenticacion-demo-aa645-default-rtdb.firebaseio.com",
+    projectId: "autenticacion-demo-aa645",
+    storageBucket: "autenticacion-demo-aa645.appspot.com",
+    messagingSenderId: "322673810048",
+    appId: "1:322673810048:web:20f9b0bb8e32fc3f162465",
+    measurementId: "G-DVTS2ER1X8"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyCRsfeWUBi6hnXS1jaGojG9J8XJ6SgvhkY",
+    authDomain: "autenticacion-demo-aa645.firebaseapp.com",
+    databaseURL: "https://autenticacion-demo-aa645-default-rtdb.firebaseio.com",
+    projectId: "autenticacion-demo-aa645",
+    storageBucket: "autenticacion-demo-aa645.appspot.com",
+    messagingSenderId: "322673810048",
+    appId: "1:322673810048:web:20f9b0bb8e32fc3f162465",
+    measurementId: "G-DVTS2ER1X8"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
